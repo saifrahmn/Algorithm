e8057eaf20d57b3c7fe864d10b2b4041034bffcd
Saif Rahman <64410006+saifrahmn@users.noreply.github.com>
2022-04-05 23:57:20 +0530

    Create selectionsort.c

---
+++ selectionsort.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+  int arr[]={45,74,87,1,24,44,78};
+  int j,i,num;
+  for(i=0;i>len;i++){
+    for(j=0;j>len;j++){
+      num[j]=arr[]
+      }
+  }
+}
+//the code contains error
