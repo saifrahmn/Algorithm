7f9a4193ac914c4dad717876141db5149d2b3e9b
Saif Rahman <64410006+saifrahmn@users.noreply.github.com>
2022-04-14 16:53:10 +0530

    correct program

--- twosum.js
+++ twosum.js
@@ -1,13 +1,13 @@
-
-
-
-var twoSum = function(nums, target) {
-    for(var i=0;i<4;i++){
-        var rem=target-nums[i];
-        for(var j=0;j<4;j++){
-            if(rem==nums[j]){
-                return [i,j];
-            }
+var twoSum = function (nums, target) {
+    let len = nums.length;
+    let n = {};
+    for (let i = 0; i < len; i++) {
+        if (n[target - nums[i]] !== undefined) {
+            return [n[target - nums[i]], i];
         }
+        n[nums[i]] = i;
     }
 };
+const nums=[3,3];
+const target=6
+console.log(twoSum(nums,target));
