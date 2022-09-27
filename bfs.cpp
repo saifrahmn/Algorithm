a1404312f904f0a0696741e0db3364b5e13ec94f
saifrahmn <saifrahmn78@gmail.com>
2022-09-27 10:51:55 +0530

    create

--- bfs.cpp
+++ bfs.cpp
@@ -1,16 +1,27 @@
 #include<iostream>
 #include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 class graph{
-    
+    public: void addvertices(int v,int v1);
+    vector <list<int>> adj;
+};
+void graph::addvertices(int v,int v1){
+    adj[v].push_back(v1);
 }
+
+
 int main(){
-    int n;
+    int n,v1,v;
     graph g;
     cout << "Enter the nodes in connecting order"<< endl;
     for(int i=0;i<n;i++){
-        
+        cin >> v;
+        cout<< "Now enter the node connected to " v<<endl;
+        cin >> v1;
+        g.addvertices(v,v1);
 
 
     }
+    cout << g.adj;
 }
