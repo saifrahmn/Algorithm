c415a9a4effee11306bc3d9eb9350f01256230cc
saifrahmn <saifrahmn78@gmail.com>
2022-09-27 10:51:09 +0530

    creating

---
+++ bfs.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class graph{
+    
+}
+int main(){
+    int n;
+    graph g;
+    cout << "Enter the nodes in connecting order"<< endl;
+    for(int i=0;i<n;i++){
+        
+
+
+    }
+}
